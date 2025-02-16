cb924c7814a6a758668e172957b9bdc797e81dd1
Jorrel Tigbayan <jorrel6130@gmail.com>
2025-02-16 03:14:20 -0500

    Creating index.js with express/graphql boiler plate code

---
+++ index.js
@@ -0,0 +1,41 @@
+// Jorrel Tigbayan
+// 101329925    
+
+const express = require('express')
+const mongoose = require('mongoose')
+const { buildSchema } = require('graphql')
+const { graphqlHTTP } = require("express-graphql")
+
+const app = express()
+const SERVER_PORT = 6130
+
+// code
+
+const connectDB = async() => {
+    try{
+        console.log(`Attempting to connect to DB`);
+        const DB_NAME = ""
+        const DB_USER_NAME = "jorrel6130"
+        const DB_PASSWORD = 'M2s8QblJdWZUUXOD'
+        const CLUSTER_ID = 'kua6u'
+        const DB_CONNECTION = `mongodb+srv://${DB_USER_NAME}:${DB_PASSWORD}@cluster0.${CLUSTER_ID}.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+
+        mongoose.connect(DB_CONNECTION, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then( () => {
+            console.log(`MongoDB connected`)
+        }).catch( (err) => {
+            console.log(`Error while connecting to MongoDB : ${JSON.stringify(err)}`)
+        });
+    }catch(error){
+        console.log(`Unable to connect to DB : ${error.message}`);
+        
+    }
+}
+
+app.listen(SERVER_PORT, () => {
+    console.log('Server started')
+    connectDB()
+    console.log('http://localhost:6130/')
+})
