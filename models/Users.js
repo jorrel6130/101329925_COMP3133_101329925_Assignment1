60eb26f35535a5730ad179fbf32f600e4d56027a
Jorrel Tigbayan <jorrel6130@gmail.com>
2025-02-16 03:33:34 -0500

    Created users model

---
+++ models/Users.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: [true, "Emails must be unique"],
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    created_at: {
+        type: Date, 
+        default: Date.now
+    },
+    updated_at: {
+        type: Date, 
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model("user", userSchema)
