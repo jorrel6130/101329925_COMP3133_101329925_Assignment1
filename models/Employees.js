d76c8838fc2833b0fa06ed744ca8bc1587509536
Jorrel Tigbayan <jorrel6130@gmail.com>
2025-04-06 04:37:39 -0400

    Added database connection to models instead

--- models/Employees.js
+++ models/Employees.js
@@ -3,6 +3,22 @@
 
 const mongoose = require('mongoose')
 
+try {
+        console.log(`Attempting to connect to DB`);
+        const DB_CONNECTION = `mongodb+srv://vercel-admin-user:4H16DPth9NEoOlnZ@cluster0.vd5kp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+        mongoose.connect(DB_CONNECTION, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(() => {
+            console.log(`MongoDB connected`)
+        }).catch((err) => {
+            console.log(`Error while connecting to MongoDB : ${JSON.stringify(err)}`)
+        });
+    } catch (error) {
+        console.log(`Unable to connect to DB : ${error.message}`);
+    }
+
 const employeeSchema = new mongoose.Schema({
     first_name: {
         type: String,
